a3826c0b9bf400241ba787644dbb63ba87a194aa
pavel.mikhailov <pamix1985@gmail.com>
2021-08-07 00:17:38 +0700

    webpack 5 project build setup, add font family, add favicon

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import './styles/index.scss'
 const greet = 'Hello world'
 
 console.log(greet)
+
+async function start() {
+  return await Promise.resolve('async is working')
+}
+
+start().then(console.log)
+
+class Util {
+  static id = Date.now()
+}
+
+console.log('Util Id:', Util.id)
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
+  entry: ['@babel/polyfill', './index.js'],
   output: {
-    filename: '[contenthash].js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]'
+    assetModuleFilename: 'assets/[hash][ext][query]'
   },
   devServer: {
     port: 4200,
-    hot: true,
+    hot: isDev,
+  },
+  resolve: {
+    extensions: ['.js']
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -21,7 +30,15 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[contenthash].css'
+      filename: filename('css')
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]
     })
   ],
   module: {
@@ -35,12 +52,22 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpe?g|svg|gif)$/i,
+        test: /\.(png|jpe?g|svg|gif)$/i, 
         type: 'asset/resource'
       },
       {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        use: ['file-loader']
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
